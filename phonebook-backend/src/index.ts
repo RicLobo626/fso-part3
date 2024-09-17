a98476038060dd0e1140ea4b306a2c4162c30f49
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-17 23:16:39 +0100

    Change PORT

--- phonebook-backend/src/index.ts
+++ phonebook-backend/src/index.ts
@@ -99,6 +99,6 @@ app.delete("/api/persons/:id", (req, res) => {
   return res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

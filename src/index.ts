b37b0b43c541d38762cd9c11f36534e3fc2fc893
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-17 18:37:42 +0100

    Init phonebook backend

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (_req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

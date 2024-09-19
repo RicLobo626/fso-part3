06545045f27d72b27ece30ab17e80c52da7eec3b
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-19 18:18:59 +0100

    Add db module

---
+++ phonebook-backend/src/utils/db.ts
@@ -0,0 +1,12 @@
+import { connect, set } from "mongoose";
+
+export const connectToDB = async () => {
+  try {
+    await connect(process.env.MONGODB_URI!);
+    console.log("Connected to DB");
+  } catch (e) {
+    console.error("Couldn't connect to DB: ", e);
+  }
+};
+
+set("strictQuery", false);
